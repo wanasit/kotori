aa64ec43d1ae8e397a790e654fc78fabb369193f
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2020-06-28 22:47:49 +0900

    Start publishing kotori-sudachi

--- kotori-sudachi/build.gradle.kts
+++ kotori-sudachi/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-library`
     kotlin("jvm")
+    `maven-publish`
 }
 
 dependencies {
@@ -16,4 +17,41 @@ dependencies {
 
 tasks.withType<Test> {
     dependsOn(":prepareTestingData")
+}
+
+task<Jar>("sourcesJar") {
+    from(sourceSets["main"].allSource)
+    archiveClassifier.set("sources")
+}
+
+publishing {
+    publications {
+        val sourcesJar by tasks
+        register(project.name, MavenPublication::class) {
+            groupId = project.group as String
+            version = project.version as String?
+            artifactId = project.name
+
+            from(components["java"])
+            artifact(sourcesJar)
+
+            pom {
+                name.set(project.name)
+                description.set(Kotori.Package.desc)
+                url.set(Kotori.Package.url)
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                    }
+                }
+                scm {
+                    url.set(Kotori.Package.url)
+                    connection.set(Kotori.Package.scm)
+                    developerConnection.set(Kotori.Package.scm)
+                }
+            }
+        }
+    }
 }
