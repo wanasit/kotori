7ca4f352e6bf4fb314828d36a217953ec5006b20
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2020-08-30 15:18:11 +0900

    Fix gradle publishing script

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,18 @@ plugins {
     id(Release.Bintray.plugin) version Release.Bintray.version
 }
 
-val versionDetails: groovy.lang.Closure<*> by extra
-val gitVersionDetails: VersionDetails = versionDetails() as VersionDetails
+// Do not publish root project
+bintray {
+    setPublications()
+    setConfigurations()
+    with(pkg) {
+        repo = Kotori.Package.repo
+        name = Kotori.Package.name
+    }
+}
+
 
 allprojects {
-    version = gitVersionDetails.lastTag
-    group = Kotori.groupId
-
     repositories {
         jcenter()
     }
@@ -27,33 +32,81 @@ allprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
     }
+}
 
-    apply {
-        plugin(Release.MavenPublish.plugin)
-        plugin(Release.Bintray.plugin)
-    }
+val versionDetails: groovy.lang.Closure<*> by extra
+val gitVersionDetails: VersionDetails = versionDetails() as VersionDetails
+val publishProjectPaths = listOf(":kotori", ":kotori-sudachi")
+subprojects {
+    version = gitVersionDetails.lastTag
+    group = Kotori.groupId
 
-    bintray {
-        user = findProperty("BINTRAY_USER") as? String
-        key = findProperty("BINTRAY_KEY") as? String
-        setPublications(project.name)
-        with(pkg) {
-            repo = Kotori.Package.repo
-            name = Kotori.Package.name
-            desc = Kotori.Package.desc
-            userOrg = Kotori.Package.userOrg
-            websiteUrl = Kotori.Package.url
-            vcsUrl = Kotori.Package.url
-            setLicenses(Kotori.Package.licenseName)
-            with(version) {
-                name = project.version as String
+    if (project.path in publishProjectPaths) {
+
+        apply {
+            plugin("java-library")
+            plugin(Release.MavenPublish.plugin)
+            plugin(Release.Bintray.plugin)
+        }
+
+        task<Jar>("sourcesJar") {
+            from(sourceSets["main"].allSource)
+            archiveClassifier.set("sources")
+        }
+
+
+        bintray {
+            user = findProperty("BINTRAY_USER") as? String
+            key = findProperty("BINTRAY_KEY") as? String
+            publish = true
+            override = true
+
+            setPublications(project.name)
+            with(pkg) {
+                repo = Kotori.Package.repo
+                name = Kotori.Package.name
+                desc = Kotori.Package.desc
+                userOrg = Kotori.Package.userOrg
+                websiteUrl = Kotori.Package.url
+                vcsUrl = Kotori.Package.url
+                setLicenses(Kotori.Package.licenseName)
+                with(version) {
+                    name = project.version as String
+                }
             }
         }
-    }
 
-    publishing {
-        publications {
-            // Don't publish any, unless override in sub-project
+        val sourcesJar by tasks
+        publishing {
+            publications {
+                register(project.name, MavenPublication::class) {
+                    from(components["java"])
+                    artifact(sourcesJar)
+
+                    groupId = project.group as String
+                    version = project.version as String?
+                    artifactId = project.name
+
+                    pom {
+                        name.set(project.name)
+                        description.set(Kotori.Package.desc)
+                        packaging = "jar"
+                        url.set(Kotori.Package.url)
+
+                        licenses {
+                            license {
+                                name.set("MIT License")
+                                url.set("http://www.opensource.org/licenses/mit-license.php")
+                            }
+                        }
+                        scm {
+                            url.set(Kotori.Package.url)
+                            connection.set(Kotori.Package.scm)
+                            developerConnection.set(Kotori.Package.scm)
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- kotori/build.gradle.kts
+++ kotori/build.gradle.kts
@@ -4,47 +4,10 @@ plugins {
     `maven-publish`
 }
 
-task<Jar>("sourcesJar") {
-    from(sourceSets["main"].allSource)
-    archiveClassifier.set("sources")
-}
-
 dependencies {
     implementation(Kotlin.Dependencies.Stdlib)
 
     testImplementation(Kotlin.Dependencies.Test)
     testImplementation(Kotlin.Dependencies.TestJunit)
     testImplementation(Kuromoji.Dependencies.Kuromoji_IPADIC)
-}
-
-publishing {
-    publications {
-        val sourcesJar by tasks
-        register(project.name, MavenPublication::class) {
-            groupId = project.group as String
-            version = project.version as String?
-            artifactId = project.name
-
-            from(components["java"])
-            artifact(sourcesJar)
-
-            pom {
-                name.set(project.name)
-                description.set(Kotori.Package.desc)
-                url.set(Kotori.Package.url)
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("http://www.opensource.org/licenses/mit-license.php")
-                    }
-                }
-                scm {
-                    url.set(Kotori.Package.url)
-                    connection.set(Kotori.Package.scm)
-                    developerConnection.set(Kotori.Package.scm)
-                }
-            }
-        }
-    }
 }
